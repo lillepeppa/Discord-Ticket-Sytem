6a9839ef23a007a05572015d934744b35add61b1
lillepeppa <totharmin20081112@gmail.com>
2025-06-10 23:21:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     modmail: {
-        ownerID: '', // ← Insert the Server Owner ID.
-        supportId: '', // ← Insert General Support Category ID.
-        premiumId: '', // ← Insert Premium Support Category ID.
-        whitelist: '', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
-        staff: '', // ← Insert Staff Role to be pinged upon ticket creation.
+        ownerID: '882900421868417055', // ← Insert the Server Owner ID.
+        supportId: '1382093672958398705', // ← Insert General Support Category ID.
+        premiumId: '1382093672958398705', // ← Insert Premium Support Category ID.
+        whitelist: '1382101568081367051', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
+        staff: '1382101568081367051', // ← Insert Staff Role to be pinged upon ticket creation.
     },
     logs: {
-        logschannel: '' // ← Insert Ticket Logs Channel ID.
+        logschannel: '1382094183946387577' // ← Insert Ticket Logs Channel ID.
     }
 };
